8a7e057968226c4542f98687beb6ceccc5f2210e
Smirnov Sergey A <ssmirnov@megafon.ru>
2019-12-02 22:33:08 +0300

    feat(WeatherProvider): Add WeatherProvider. Upgrade getWeather method of MainController.

---
+++ src/main/java/org/serasmi/learning/weather/WeatherProvider.java
@@ -0,0 +1,33 @@
+package org.serasmi.learning.weather;
+
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Component
+public class WeatherProvider {
+
+  public String getWeather(String sUrl) throws IOException {
+    URL url = new URL(sUrl);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Content-Type", "application/json");
+
+    try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+      String inputLine;
+      final StringBuilder content = new StringBuilder();
+      while ((inputLine = in.readLine()) != null) {
+        content.append(inputLine);
+      }
+      return content.toString();
+    } catch (final Exception ex) {
+      ex.printStackTrace();
+      return null;
+    }
+  }
+}
--- src/main/java/org/serasmi/learning/weather/controllers/MainController.java
+++ src/main/java/org/serasmi/learning/weather/controllers/MainController.java
@@ -1,12 +1,17 @@
 package org.serasmi.learning.weather.controllers;
 
+import org.serasmi.learning.weather.WeatherProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+
 @Controller
 public class MainController {
+
+  private WeatherProvider weatherProvider;
 
   @GetMapping("/weather")
   String weather(@RequestParam(name="city", required=false, defaultValue="Moscow") String city,
@@ -14,6 +19,17 @@ public class MainController {
                  Model model) {
     model.addAttribute("city", city);
     model.addAttribute("service", service);
+
+    String result = "";
+
+    try {
+      // TODO: Change API entrypoint. Needs to use API according to 'service' parameter.
+      result = weatherProvider.getWeather("http://jsonplaceholder.typicode.com/posts?_limit=10%22");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    model.addAttribute("weather", result);
 
     return "weather";
   }
